446d600a2171eb62ef74fcd9c48e054d57727e39
Ivan Filho <i@ivanfilho.com>
2024-05-12 06:00:36 -0300

    docs: update readme

--- app/forms/hello/schema.ts
+++ app/forms/hello/schema.ts
@@ -4,6 +4,6 @@ export const helloSchema = z.object({
   name: z.string().min(3, 'Name must be at least 3 characters'),
   age: z.number().int().min(18, 'You must be at least 18 years old'),
   message: z.string().min(10, 'Message must be at least 10 characters'),
-  attachment: z.instanceof(File).nullable().optional(),
+  attachment: z.instanceof(File).nullish(),
   terms: z.boolean().refine((value) => value, 'You must accept the terms')
 })
